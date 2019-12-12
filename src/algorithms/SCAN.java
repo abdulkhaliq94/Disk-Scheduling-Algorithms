8131d92fa7c9213981e54271f88f4a9851614df0
Abdul Khaliq <abdulkhaliq94@gmail.com>
2019-12-12 01:40:48 -0500

    fixed up code

--- src/algorithms/SCAN.java
+++ src/algorithms/SCAN.java
@@ -22,9 +22,11 @@ public class SCAN {
         int[] values = new int[num];
         int var = 0, distance = 0, i, l, f = 0;
         System.out.println("Enter Values Of Cylinders: ");
-        for(i=0;i<num;i++) values[i] = input.nextInt();
+        for(i = 0; i < num; i++)
+            values[i] = input.nextInt();
+
         int[] done = new int[num+1];
-        for(i=0;i<num;i++)
+        for(i = 0; i < num; i++)
         {
             int r = -1;
             int min = 10000;
@@ -32,33 +34,33 @@ public class SCAN {
             {
                 if(f == 0)
                 {
-                    if(values[l]>head && min>Math.abs(values[l]-head) && done[l]==0)
+                    if(values[l] > head && min > Math.abs(values[l] - head) && done[l] == 0)
                     {
                         min = Math.abs(values[l] - head);
                         r = l;
                     }
                 }
 
-                else if(f==1)
+                else if(f == 1)
                 {
-                    if(values[l]<=head && min>Math.abs(values[l]-head) && done[l]==0)
+                    if(values[l] <= head && min > Math.abs(values[l] - head) && done[l] == 0)
                     {
                         r = l;
-                        min = Math.abs(values[l]-head);
+                        min = Math.abs(values[l] - head);
                     }
                 }
             }
-            if(r==-1)
+            if(r == -1)
             {
-                if(f==0)
+                if(f == 0)
                 {
-                    f=1;
+                    f = 1;
                     distance += Math.abs(tail - head);
                     head = tail;
                 }
                 else
                 {
-                    f=0;
+                    f = 0;
                     distance += Math.abs(0 - head);
                     head = 0;
                 }
@@ -69,6 +71,6 @@ public class SCAN {
             distance += Math.abs(values[r] - head);
             head = values[r];
         }
-        System.out.println("Time it takes: "+distance);
+        System.out.println("Time it takes: " + distance);
     }
 }
